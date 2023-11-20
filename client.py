02c3e52ccfa8c949ba1acb153acfe55066b77754
Abdullah Shafique <abdshafique@hotmail.com>
2023-11-20 16:48:52 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,29 +1,55 @@
 import socket
+import threading
+import tkinter as tk
+from tkinter import scrolledtext
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_ip = "192.168.50.198"
-server_port = 9998
 
-client.connect((server_ip, server_port))
+class ChatClient:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Instant Messaging - User2")
 
-done = False
+        self.text_area = scrolledtext.ScrolledText(root)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state="disabled")
 
-while not done:
-    # Send messages to user1
-    user2_message = input("User2 Message: ")
-    client.send(user2_message.encode('utf-8'))
+        self.msg_entry = tk.Entry(root)
+        self.msg_entry.pack(padx=20, pady=5)
+        self.msg_entry.bind("<Return>", self.send_message)
 
-    if user2_message == 'quit':
-        print("User2 has exited. Closing connection.")
-        break
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_ip = ""
+        self.server_port = 9998
+        self.client.connect((self.server_ip, self.server_port))
 
-    # Receive replies from user1
-    user1_reply = client.recv(1024).decode('utf-8')
+        self.receive_thread = threading.Thread(target=self.receive_messages)
+        self.receive_thread.start()
 
-    if user1_reply == 'quit':
-        print("User1 has exited. Closing connection.")
-        break
+    def send_message(self, event=None):
+        message = self.msg_entry.get()
+        self.msg_entry.delete(0, tk.END)
+        if message == "quit":
+            self.client.close()
+            self.root.quit()
+        else:
+            self.client.send(message.encode("utf-8"))
 
-    print(f"User1 Reply: {user1_reply}")
+    def receive_messages(self):
+        while True:
+            try:
+                msg = self.client.recv(1024).decode("utf-8")
+                if msg == "quit":
+                    self.client.close()
+                    break
+                self.text_area.config(state="normal")
+                self.text_area.insert("end", f"User1 Reply: {msg}\n")
+                self.text_area.yview("end")
+                self.text_area.config(state="disabled")
+            except Exception as e:
+                print("An error occurred:", e)
+                break
 
-client.close()
+
+root = tk.Tk()
+app = ChatClient(root)
+root.mainloop()
