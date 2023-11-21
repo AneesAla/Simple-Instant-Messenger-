c9edf871177d8c9f676893e14b2a1da8a48253e1
Abdullah Shafique <abdshafique@hotmail.com>
2023-11-20 19:38:10 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,34 +1,33 @@
 import socket
+import threading
+
+
+def client_thread(conn, addr):
+    while True:
+        try:
+            message = conn.recv(1024).decode("utf-8")
+            if message == "quit":
+                print("Closing connection.")
+                break
+            if message:
+                print(f"{addr}: {message}")
+                conn.sendall(f"Server received: {message}".encode())
+            else:
+                break
+        except:
+            break
+
+    conn.close()
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("192.168.50.198", 9998))
+server.bind(("", 9998))
 server.listen()
-
-print("Waiting for user2 to connect...")
-
-client, addr = server.accept()
-print(f"User2 connected from {addr}")
 
 done = False
 
 while not done:
-    # Receive messages from user2
-    user2_message = client.recv(1024).decode('utf-8')
-
-    if user2_message == 'quit':
-        print("User2 has exited. Closing connection.")
-        break
-
-    print(f"User2: {user2_message}")
-
-    # Reply to user2
-    user1_reply = input("User1 Reply: ")
-    client.send(user1_reply.encode('utf-8'))
-
-    if user1_reply == 'quit':
-        print("User1 has exited. Closing connection.")
-        break
-
-client.close()
-server.close()
-
+    client, addr = server.accept()
+    print(f"Connected with {addr}")
+    thread = threading.Thread(target=client_thread, args=(client, addr))
+    thread.start()
