e3559cfcf8d11abf1d11f23a0fd9ea2e51856164
AneesAla <93885287+AneesAla@users.noreply.github.com>
2023-11-26 22:07:18 -0600

    Add files via upload

--- client.py
+++ client.py
@@ -1,67 +1,39 @@
 import socket
 import threading
-import tkinter as tk
-from tkinter import scrolledtext
 
+# Choosing Nickname
+nickname = input("Choose your nickname: ")
 
-class ChatClient:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Instant Messaging - User2")
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55557))
 
-        self.text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD)
-        self.text_area.pack(padx=20, pady=5, expand=True, fill="both")
-        self.text_area.config(state="disabled", font=("Arial", 12))
+# Listening to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            # Close Connection When Error
+            print("An error occured!")
+            client.close()
+            break
 
-        self.msg_frame = tk.Frame(root)
-        self.msg_frame.pack(padx=20, pady=5, fill=tk.X)
+ # Sending Messages To Server
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
 
-        self.msg_entry = tk.Entry(self.msg_frame, font=("Arial", 12))
-        self.msg_entry.pack(side=tk.LEFT, expand=True, fill=tk.X)
-        self.msg_entry.bind("<Return>", self.send_message)
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
 
-        self.send_button = tk.Button(
-            self.msg_frame, text="Send", command=self.send_message
-        )
-        self.send_button.pack(side=tk.RIGHT, padx=(5, 0))
-
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_ip = ""
-        self.server_port = 9998
-        self.client.connect((self.server_ip, self.server_port))
-
-        self.receive_thread = threading.Thread(target=self.receive_messages)
-        self.receive_thread.start()
-
-    def send_message(self, event=None):
-        message = self.msg_entry.get()
-        self.msg_entry.delete(0, tk.END)
-        if message == "quit":
-            self.client.close()
-            self.root.quit()
-        else:
-            self.client.send(message.encode("utf-8"))
-            self.update_chat_window("You", message)
-
-    def receive_messages(self):
-        while True:
-            try:
-                msg = self.client.recv(1024).decode("utf-8")
-                if msg == "quit":
-                    self.client.close()
-                    break
-                self.update_chat_window("User1", msg)
-            except Exception as e:
-                print("An error occurred:", e)
-                break
-
-    def update_chat_window(self, sender, message):
-        self.text_area.config(state="normal")
-        self.text_area.insert("end", f"{sender}: {message}\n")
-        self.text_area.yview("end")
-        self.text_area.config(state="disabled")
-
-
-root = tk.Tk()
-app = ChatClient(root)
-root.mainloop()
+write_thread = threading.Thread(target=write)
+write_thread.start()
--- server.py
+++ server.py
@@ -1,33 +1,62 @@
 import socket
 import threading
 
+# Connection Data
+host = '127.0.0.1'
+port = 55557
 
-def client_thread(conn, addr):
-    while True:
-        try:
-            message = conn.recv(1024).decode("utf-8")
-            if message == "quit":
-                print("Closing connection.")
-                break
-            if message:
-                print(f"{addr}: {message}")
-                conn.sendall(f"Server received: {message}".encode())
-            else:
-                break
-        except:
-            break
-
-    conn.close()
-
-
+# Starting up the Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("", 9998))
+server.bind((host, port))
 server.listen()
 
-done = False
+# Lists For Clients and nicknames
+clients = []
+nicknames = []
 
-while not done:
-    client, addr = server.accept()
-    print(f"Connected with {addr}")
-    thread = threading.Thread(target=client_thread, args=(client, addr))
-    thread.start()
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+receive()
